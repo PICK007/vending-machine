2ead754bbab597eca122d9921ebdcb6c52e369b7
pick <thanawat99a11@gmail.com>
2026-09-07 21:46:33 +0700

    Connect Railway MySQL

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ app.use(express.static(__dirname));
 // ==================================================
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "vending_machine"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: Number(process.env.MYSQLPORT)
 });
 
 db.connect((err) => {
